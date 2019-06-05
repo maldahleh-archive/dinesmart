0ce231979f5130643e21c98194a43b65977067a2
Mohammed Al-Dahleh <maldahleh@gmail.com>
2019-06-04 23:27:09 -0400

    Initial Commit

---
+++ dinesmart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dinesmart
+//
+//  Created by Mohammed Al-Dahleh on 2019-06-04.
+//  Copyright © 2019 Codeovo Software Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
