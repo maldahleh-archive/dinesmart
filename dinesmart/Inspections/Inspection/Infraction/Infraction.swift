fe3cd0209c68b8ac26647100e89d98df04155e38
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-11-01 14:31:03 -0400

    Updates models for Realm

--- dinesmart/Inspections/Inspection/Infraction/Infraction.swift
+++ dinesmart/Inspections/Inspection/Infraction/Infraction.swift
@@ -1,32 +1,6 @@
-import Realm
 import RealmSwift
 
-@objcMembers class Infraction: Object, Decodable {
-    dynamic var details = ""
-    dynamic var severity = ""
-    
-    private enum CodingKeys: String, CodingKey {
-        case details, severity
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.details = try container.decode(String.self, forKey: .details)
-        self.severity = try container.decode(String.self, forKey: .severity)
-        
-        super.init()
-    }
-    
-    required init() {
-        super.init()
-    }
-    
-    required init(value: Any, schema: RLMSchema) {
-        super.init(value: value, schema: schema)
-    }
-    
-    required init(realm: RLMRealm, schema: RLMObjectSchema) {
-        super.init(realm: realm, schema: schema)
-    }
+class Infraction: Object, Decodable {
+    @Persisted var details = ""
+    @Persisted var severity = ""
 }
--- dinesmart/Inspections/Location/Coordinate.swift
+++ dinesmart/Inspections/Location/Coordinate.swift
@@ -1,35 +1,9 @@
 import MapKit
-import Realm
 import RealmSwift
 
-@objcMembers class Coordinate: Object, Decodable {
-    dynamic var lat = ""
-    dynamic var lon = ""
-    
-    private enum CodingKeys: String, CodingKey {
-        case lat, lon
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.lat = try container.decode(String.self, forKey: .lat)
-        self.lon = try container.decode(String.self, forKey: .lon)
-        
-        super.init()
-    }
-    
-    required init() {
-        super.init()
-    }
-    
-    required init(value: Any, schema: RLMSchema) {
-        super.init(value: value, schema: schema)
-    }
-    
-    required init(realm: RLMRealm, schema: RLMObjectSchema) {
-        super.init(realm: realm, schema: schema)
-    }
+class Coordinate: Object, Decodable {
+    @Persisted var lat = ""
+    @Persisted var lon = ""
 }
 
 // MARK: - Mapping
